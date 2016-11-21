eecbc5f40fa483701470acc0b371caa2171deda4
petervkay <petervkay@gmail.com>
2016-11-21 09:42:15 -0600

    fixed selector names

--- cstdnt-admin-styles.php
+++ cstdnt-admin-styles.php
@@ -1,30 +1,30 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit; 
-add_action('admin_head', 'cstdnt_admin_styles');
-add_action('admin_head', 'cstdnt_admin_scripts');
+add_action('admin_head', 'my_custom_styles');
+add_action('admin_head', 'my_custom_scripts');
 
-function cstdnt_admin_styles() {
+function my_custom_styles() {
   echo '
   <style>
-    #cd-admin input.wide {
-    	width:500px;
+    #cstdnt-admin input.wide {
+      width:500px;
     }
 
-    .cd_form_section {
-    	background-color:#b5b5b5; 
-    	margin-bottom:30px;
-    	padding:30px;
+    .cstdnt_form_section {
+      background-color:#b5b5b5; 
+      margin-bottom:30px;
+      padding:30px;
     }
 
 
   </style>';
 }
 
-function cstdnt_admin_scripts() {
-	echo '
-	<script>
-		jQuery(document).ready(function($) {
-      $(".cd_form_section .enable").each(function() {
+function my_custom_scripts() {
+  echo '
+  <script>
+    jQuery(document).ready(function($) {
+      $(".cstdnt_form_section .enable").each(function() {
         $row = $(this).closest("tr").siblings();
         
         if ($(this).is(":checked")) {
@@ -38,12 +38,12 @@ function cstdnt_admin_scripts() {
       });
     
 
-			$(".cd_form_section .enable").click(function() {
-				$row = $(this).closest("tr").siblings();
-				$row.toggle();
-			});
-		});
-	</script>';
+      $(".cstdnt_form_section .enable").click(function() {
+        $row = $(this).closest("tr").siblings();
+        $row.toggle();
+      });
+    });
+  </script>';
 }
 
 
